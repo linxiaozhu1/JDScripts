ab92f9dff23009677fa690acc21c711b4a7c6e1e
林小猪 <52615150+linxiaozhu1@users.noreply.github.com>
2021-11-22 12:32:04 +0800

    Rename bili.sh to bilili.sh

--- bili.sh
+++
@@ -1,1 +0,0 @@
-java -jar /ql/scripts/bilibili/BILIBILI-HELPER.jar /ql/scripts/bilibili/config.json
---
+++ bilili.sh
@@ -0,0 +1,1 @@
+java -jar /ql/scripts/bilibili/BILIBILI-HELPER.jar /ql/scripts/bilibili/config.json
